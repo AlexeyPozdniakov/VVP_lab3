5dd52db240bd9a36999ad22aafb8dc015535e43c
AlexSmith1234 <54896619+AlexSmith1234@users.noreply.github.com>
2020-11-09 16:52:06 +0300

    Лабораторная 3

---
+++ lab3.4.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main()
+{
+    int x;
+    cout<<"Input X"<<endl;
+    cin>>x;
+    cout<<"Y="<<3*pow(x,6)-6*pow(x,2)-7<<endl;
+}
---
+++ lab3.6.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    float a,b;
+    cout<<"Input number"<<endl;
+    cin<<b;
+    b*=b;
+    a=b*b;
+    a*=a;
+    cout<<a<<endl;
+}
